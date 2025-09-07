44b3c2808ec45472315e0c9d4259f90135bdc98d
Frank Escramosa A99 <frank.escramosa@august99.com>
2025-09-07 14:38:54 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+    return (
+        <div className="h-screen flex items-center justify-center bg-black">
+            <h1 className="text-5xl font-extrabold text-yellow-400">
+                Hello My Lord 👑 — Tailwind Works!
+            </h1>
+        </div>
+    )
+}
+
+export default App
